d498edd7e3e53e963ffba7c66d1da01426fb5ab6
eirc winkel <eric0077winkel@gmail.com>
2016-04-08 10:51:14 +0200

    This is node shop project.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# test
-this is a test repository
+# shopping
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/News1Controller.js
@@ -0,0 +1,11 @@
+/**
+ * News1Controller
+ *
+ * @description :: Server-side logic for managing news1s
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
